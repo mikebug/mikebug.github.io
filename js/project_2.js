ba778486deb7aa7cd5c2294d1f27d9f618a88524
Mike Buglione <mbuglion@stevens.edu>
2020-04-28 22:41:29 -0400

    pls no more

--- js/project_2.js
+++ js/project_2.js
@@ -1,5 +1,5 @@
 const element = document.getElementById("body");
-element.classList.add("animated", "fadeInUp");
+element.classList.add("animated", "fadeIn");
 document.getElementById("link").addEventListener("click", function () {
   const element = document.getElementById("about");
   element.classList.add("animated", "fadeOutDown");
